efa399848197ce6cf4c40fbf18460f465dc3c845
Saida Davis <saihdavis@gmail.com>
2020-05-04 05:20:16 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,36 @@
+require "pry"
 class Owner
-  # code goes here
+  
+  attr_reader :name, :species
+  @@all = []
+  
+  def initialize(name)
+    @name= name
+    @species = "human"
+  end
+  
+  def save
+    @@all << self
+  end
+  
+  def self.all
+    #binding.pry
+    @@all 
+  end
+  
+  def say_species 
+    "I am a #{species}."
+end
+  
+  def cats 
+    Cat.all.select {|cat| cat.owner == self}
+  end
+  
+  def dogs 
+    Dog.all.select {|dog| dog.owner == self}
+  end
+  
+  def self.count
+    @all.count
+  end
 end
