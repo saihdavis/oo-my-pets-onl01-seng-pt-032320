cd42ca99b677f0b663f373a29f640ab100b4ae8f
Saida Davis <saihdavis@gmail.com>
2020-05-04 05:32:15 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,38 +1,67 @@
-require "pry"
 class Owner
-  
+  # code goes here
   attr_reader :name, :species
   @@all = []
-  
+
   def initialize(name)
-    @name= name
+    @name = name
     @species = "human"
+    save
   end
-  
+
   def save
     @@all << self
   end
-  
-  
-  
-  def say_species 
+
+  def say_species
     "I am a #{species}."
-end
-  
-  def cats 
-    Cat.all.select {|cat| cat.owner == self}
   end
-  
-  def dogs 
-    Dog.all.select {|dog| dog.owner == self}
+
+  def cats
+    Cat.all.select {|kitty| kitty.owner == self}
   end
-  
+
+  def dogs
+    Dog.all.select {|pup| pup.owner == self}
+  end
+
+  def buy_cat(name)
+    Cat.new(name, self)
+  end
+
+  def buy_dog(name)
+    Dog.new(name, self)
+  end
+
+  def walk_dogs
+    self.dogs.each {|pups| pups.mood = "happy"}
+  end
+
+  def feed_cats
+    self.cats.each {|kitty| kitty.mood = "happy"}
+  end
+
+  def sell_pets
+    pets = self.dogs + self.cats
+    pets.each do |pet|
+      pet.mood = "nervous"
+      pet.owner = nil
+    end
+  end
+
+  def list_pets
+    "I have #{self.dogs.count} dog(s), and #{self.cats.count} cat(s)."
+  end
+
   def self.all
-    #binding.pry
-    @@all 
+    @@all
   end
-  
+
   def self.count
-    @all.count
+    @@all.count
+  end
+
+  def self.reset_all
+    @@all.clear
   end
 end
